148d5023dab03e5ed2e7f12296202ef57e997f5e
ekolev11 <evgenikolev11@gmail.com>
2023-08-29 21:55:56 +0300

    update

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,16 @@
-function bmiCalc(weight, height){
-    var result = weight / (height * height);
-    return Math.round(result);
+function isLeap(year){
+    if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0){
+        return "Leap";
+    }
+    if(year % 4 === 0 && year % 100 === 0){
+        return "Not Leap";
+    }
+    if(year % 4 === 0){
+        return "Leap";
+    }else{
+        return "Not Leap year."
+    }
 }
-var bmi = bmiCalc(65, 1.8)
-console.log(bmi); 
+var yearType = isLeap(20)
+console.log(yearType);
+
