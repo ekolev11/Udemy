84dd5c0991fadc3ac4e8a9ad790d8b40c9826795
ekolev11 <evgenikolev11@gmail.com>
2023-09-09 22:59:24 +0300

    drum kit

---
+++ JS/Drum Kit Starting Files/index.js
@@ -0,0 +1,9 @@
+const buttons = document.querySelectorAll(".drum");
+buttons.forEach(function(element){
+    element.addEventListener("click", function(){
+        alert("i have been clicked");
+
+    })
+
+
+});
