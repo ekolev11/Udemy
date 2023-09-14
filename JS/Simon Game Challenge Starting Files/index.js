198bd9f867e93a3485d01aaa73476124b732036d
ekolev11 <evgenikolev11@gmail.com>
2023-09-14 17:32:34 +0300

    Simon game, working

--- JS/Simon Game Challenge Starting Files/index.js
+++ JS/Simon Game Challenge Starting Files/index.js
@@ -3,16 +3,18 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 var level = 0;
-var gameRunning = true;
-var keyPressCount = 1;
+var userTurn = false;
+var keyPressCount = 0;
 
 function nextSequence(){
+    userClickedPattern = [];
     level++
     $("h1").text("Level " + level);
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
     $("." + randomChosenColor).fadeOut(100).fadeIn(100);
+
     playSound(randomChosenColor);
     
 }
@@ -22,9 +24,12 @@ $(".btn").on("click", function (){
     userClickedPattern.push(userChosenColor);
     playSound(userChosenColor);
     animatePress(userChosenColor);
-    keyPressCount = 0;
-    var colorIndex = buttonColors.indexOf(userChosenColor);
-    checkAnswer(colorIndex)
+    if(userClickedPattern.length === gamePattern.length){
+        checkAnswer(colorIndex)
+    }
+    
+    var colorIndex = buttonColors.indexOf(userChosenColor); 
+    //checkAnswer(colorIndex)
     
     
 
@@ -62,7 +67,7 @@ function animatePress(currentColour){
     }, 100);
 }
 $(document).on('keydown', function(){
-    
+    keyPressCount++
     if(keyPressCount === 1){
         keyPressCount++
         nextSequence()
@@ -71,18 +76,27 @@ $(document).on('keydown', function(){
 });
 
 function checkAnswer(currentLevel){
-    var colorCheck = gamePattern.pop();
-    if(buttonColors[currentLevel] === colorCheck){
-        gamePattern.push(colorCheck);
-        console.log(userClickedPattern);
-        console.log(gamePattern);
+    
+    //var colorCheck = gamePattern.pop();
+    //if(buttonColors[currentLevel] === colorCheck){
+        //gamePattern.push(colorCheck);
         if(JSON.stringify(userClickedPattern) === JSON.stringify(gamePattern)){
             setTimeout(function() {
                 nextSequence()
             }, 1000);
+        }else{
+            level = 0;
+        $("h1").text("Game Over, press Any key to restart the game.")
+        keyPressCount = 0;
+        gamePattern = [];
+        userClickedPattern = [];
         }
-    }else{
-        
-    }
+    //}
+    //else{
+        //console.log("wrong");
+       // $("h1").text("Game Over")
+       // gamePattern = [];
+       // keyPressCount = 0;
+    //}
    
 }
